327afa1e6236fb296f25a134b6261ce7e0d9a528
Fyodor Zh <fese@list.ru>
2026-06-15 03:39:49 +0300

    Action 08 - Deploy and ops update

--- deploy/client/deploy-docker.sh
+++ deploy/client/deploy-docker.sh
@@ -6,9 +6,12 @@ cd "$(dirname "$0")"
 PIGEON_URL="tcp|203.0.113.10:9000/30"
 export PIGEON_URL
 
+export CLIENT_ID="${CLIENT_ID:-pp-client-1}"
+
 docker compose -f docker/docker-compose.yml build
 docker compose -f docker/docker-compose.yml up -d
 
 echo "Client deployed."
+echo "  clientId: '$CLIENT_ID'"
 echo "  url: '$PIGEON_URL'"
 echo "  logs: docker logs pigeonpost-client -f"
