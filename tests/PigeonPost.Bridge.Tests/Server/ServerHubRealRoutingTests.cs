f639580c1c2edab7c90dbcc46b431a579ba8fabb
Fyodor Zh <fese@list.ru>
2026-06-15 03:35:56 +0300

    Action 05 - Routing and validation

---
+++ tests/PigeonPost.Bridge.Tests/Server/ServerHubRealRoutingTests.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Collections.Generic;
+using Actuarius.Memory;
+using NUnit.Framework;
+using Pontifex;
+using Pontifex.Abstractions;
+using Pontifex.Abstractions.Endpoints;
+using Pontifex.Utils;
+using PigeonPost.Bridge.Protocol;
+using PigeonPost.Bridge.Server;
+using PigeonPost.Bridge.Utils;
+using Scriba;
+using Scriba.Consumers;
+
+namespace PigeonPost.Bridge.Tests.Server;
+
+[TestFixture]
+public class ServerHubRealRoutingTests
+{
+    private ServerHub _hub = null!;
+
+    [OneTimeSetUp]
+    public void SetupLogging()
+    {
+        StaticLogger.Instance.AddConsumer(new ConsoleConsumer());
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        _hub = new ServerHub(StaticLogger.Instance);
+    }
+
+    [Test]
+    public void Packet_DestinedToClientA_SentOnlyToClientA()
+    {
+        var epA = new TrackingEndpoint();
+        var epB = new TrackingEndpoint();
+
+        RegisterAndActivate("client-a", 0xC0A80101, epA);
+        RegisterAndActivate("client-b", 0xC0A80102, epB);
+
+        byte[] packet = BuildIpv4Packet(dest: 0xC0A80101, source: 0x0A000001);
+
+        _hub.OnPacketFromTun(packet);
+
+        Assert.That(_hub.DroppedNoRoute, Is.EqualTo(0));
+        Assert.That(epA.SentPackets, Has.Count.EqualTo(1));
+        Assert.That(epB.SentPackets, Has.Count.EqualTo(0));
+    }
+
+    [Test]
+    public void Packet_NoMatchingHostRoute_Dropped()
+    {
+        var epA = new TrackingEndpoint();
+        RegisterAndActivate("client-a", 0xC0A80101, epA);
+
+        byte[] packet = BuildIpv4Packet(dest: 0xC0A801FF, source: 0x0A000001);
+
+        _hub.OnPacketFromTun(packet);
+
+        Assert.That(_hub.DroppedNoRoute, Is.EqualTo(1));
+        Assert.That(epA.SentPackets, Has.Count.EqualTo(0));
+    }
+
+    [Test]
+    public void Packet_NoBroadcastBehavior()
+    {
+        var epA = new TrackingEndpoint();
+        var epB = new TrackingEndpoint();
+
+        RegisterAndActivate("client-a", 0xC0A80101, epA);
+        RegisterAndActivate("client-b", 0xC0A80102, epB);
+
+        byte[] packet = BuildIpv4Packet(dest: 0xC0A80101, source: 0x0A000001);
+
+        _hub.OnPacketFromTun(packet);
+
+        Assert.That(_hub.DroppedNoRoute, Is.EqualTo(0));
+        Assert.That(epA.SentPackets, Has.Count.EqualTo(1));
+        Assert.That(epB.SentPackets, Has.Count.EqualTo(0));
+    }
+
+    [Test]
+    public void DisconnectedClient_RouteDropped()
+    {
+        var epA = new TrackingEndpoint();
+        RegisterAndActivate("client-a", 0xC0A80101, epA);
+        _hub.RemoveSession(new ClientId("client-a"));
+
+        byte[] packet = BuildIpv4Packet(dest: 0xC0A80101, source: 0x0A000001);
+
+        _hub.OnPacketFromTun(packet);
+
+        Assert.That(_hub.DroppedNoRoute, Is.EqualTo(1));
+        Assert.That(epA.SentPackets, Has.Count.EqualTo(0));
+    }
+
+    [Test]
+    public void MalformedIpv4Packet_Dropped()
+    {
+        RegisterAndActivate("client-a", 0xC0A80101, new TrackingEndpoint());
+
+        byte[] packet = new byte[] { 0x45 };
+
+        _hub.OnPacketFromTun(packet);
+
+        Assert.That(_hub.DroppedMalformedIpv4, Is.EqualTo(1));
+    }
+
+    private void RegisterAndActivate(string clientId, uint hostIp, TrackingEndpoint endpoint)
+    {
+        var handshake = new ClientHandshake(new ClientId(clientId), hostIp);
+        var result = _hub.TryRegisterSession(handshake, out _);
+        Assert.That(result, Is.EqualTo(SessionRegistrationResult.Accepted));
+        _hub.ActivateSessionEndpoint(new ClientId(clientId), endpoint);
+    }
+
+    private static byte[] BuildIpv4Packet(uint dest, uint source)
+    {
+        var packet = new byte[20];
+        packet[0] = 0x45;
+        packet[2] = 0x00;
+        packet[3] = 20;
+        packet[12] = (byte)(source >> 24);
+        packet[13] = (byte)(source >> 16);
+        packet[14] = (byte)(source >> 8);
+        packet[15] = (byte)(source & 0xFF);
+        packet[16] = (byte)(dest >> 24);
+        packet[17] = (byte)(dest >> 16);
+        packet[18] = (byte)(dest >> 8);
+        packet[19] = (byte)(dest & 0xFF);
+        return packet;
+    }
+
+    private sealed class TrackingEndpoint : IAckRawBaseEndpoint
+    {
+        public IEndPoint? RemoteEndPoint => null;
+        public bool IsConnected => true;
+        public int MessageMaxByteSize => 1_048_576;
+        public List<byte[]> SentPackets { get; } = new();
+
+        public SendResult Send(UnionDataList bufferToSend)
+        {
+            if (bufferToSend.TryPopFirst(out IMultiRefReadOnlyByteArray? data) && data != null)
+            {
+                byte[] copy = PontifexPacketConverter.ExtractPacket(data);
+                SentPackets.Add(copy);
+            }
+            return SendResult.Ok;
+        }
+
+        public bool Disconnect(StopReason reason) => true;
+        public void GetControls(List<IControl> dst, Predicate<IControl>? predicate = null) { }
+    }
+}
---
+++ tests/PigeonPost.Bridge.Tests/Server/ServerHubRealValidationTests.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using PigeonPost.Bridge.Protocol;
+using PigeonPost.Bridge.Server;
+using Scriba;
+using Scriba.Consumers;
+
+namespace PigeonPost.Bridge.Tests.Server;
+
+[TestFixture]
+public class ServerHubRealValidationTests
+{
+    private ServerHub _hub = null!;
+
+    [OneTimeSetUp]
+    public void SetupLogging()
+    {
+        StaticLogger.Instance.AddConsumer(new ConsoleConsumer());
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        _hub = new ServerHub(StaticLogger.Instance);
+    }
+
+    [Test]
+    public void ClientPacket_SourceEqualsAdvertisedIp_Accepted()
+    {
+        var handshake = new ClientHandshake(new ClientId("client-a"), 0xC0A80101);
+        _hub.TryRegisterSession(handshake, out _);
+
+        byte[] packet = BuildIpv4Packet(source: 0xC0A80101, dest: 0x0A000001);
+
+        _hub.OnPacketFromClient(new ClientId("client-a"), packet);
+
+        Assert.That(_hub.DroppedInvalidSource, Is.EqualTo(0));
+        Assert.That(_hub.DroppedMalformedIpv4, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void ClientPacket_SourceDiffersFromAdvertisedIp_Dropped()
+    {
+        var handshake = new ClientHandshake(new ClientId("client-a"), 0xC0A80101);
+        _hub.TryRegisterSession(handshake, out _);
+
+        byte[] packet = BuildIpv4Packet(source: 0xC0A80199, dest: 0x0A000001);
+
+        _hub.OnPacketFromClient(new ClientId("client-a"), packet);
+
+        Assert.That(_hub.DroppedInvalidSource, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void ClientPacket_MalformedIpv4_Dropped()
+    {
+        var handshake = new ClientHandshake(new ClientId("client-a"), 0xC0A80101);
+        _hub.TryRegisterSession(handshake, out _);
+
+        byte[] packet = new byte[] { 0x45, 0x00 };
+
+        _hub.OnPacketFromClient(new ClientId("client-a"), packet);
+
+        Assert.That(_hub.DroppedMalformedIpv4, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void ClientPacket_NonIpv4_Dropped()
+    {
+        var handshake = new ClientHandshake(new ClientId("client-a"), 0xC0A80101);
+        _hub.TryRegisterSession(handshake, out _);
+
+        byte[] packet = new byte[20];
+        packet[0] = 0x60;
+
+        _hub.OnPacketFromClient(new ClientId("client-a"), packet);
+
+        Assert.That(_hub.DroppedMalformedIpv4, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void MultipleClients_EachValidatedSeparately()
+    {
+        _hub.TryRegisterSession(
+            new ClientHandshake(new ClientId("client-a"), 0xC0A80101), out _);
+        _hub.TryRegisterSession(
+            new ClientHandshake(new ClientId("client-b"), 0xC0A80102), out _);
+
+        byte[] validA = BuildIpv4Packet(source: 0xC0A80101, dest: 0x0A000001);
+        byte[] spoofedB = BuildIpv4Packet(source: 0xC0A80199, dest: 0x0A000002);
+        byte[] validB = BuildIpv4Packet(source: 0xC0A80102, dest: 0x0A000002);
+
+        _hub.OnPacketFromClient(new ClientId("client-a"), validA);
+        _hub.OnPacketFromClient(new ClientId("client-b"), spoofedB);
+        _hub.OnPacketFromClient(new ClientId("client-b"), validB);
+
+        Assert.That(_hub.DroppedInvalidSource, Is.EqualTo(1));
+        Assert.That(_hub.DroppedMalformedIpv4, Is.EqualTo(0));
+    }
+
+    private static byte[] BuildIpv4Packet(uint source, uint dest)
+    {
+        var packet = new byte[20];
+        packet[0] = 0x45;
+        packet[2] = 0x00;
+        packet[3] = 20;
+        packet[12] = (byte)(source >> 24);
+        packet[13] = (byte)(source >> 16);
+        packet[14] = (byte)(source >> 8);
+        packet[15] = (byte)(source & 0xFF);
+        packet[16] = (byte)(dest >> 24);
+        packet[17] = (byte)(dest >> 16);
+        packet[18] = (byte)(dest >> 8);
+        packet[19] = (byte)(dest & 0xFF);
+        return packet;
+    }
+}
